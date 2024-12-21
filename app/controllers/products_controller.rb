3e227be7668a63b006b06a65f361a73662a2074c
Patrick Orave <oravep@gmail.com>
2024-12-21 04:45:45 +0000

    10.5 Create New Product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,29 @@
 class ProductsController < ApplicationController
-	def index
-		@products = Product.all
-	end
+  def index
+    @products = Product.all
+  end
 
-	def show
-		@product = Product.find(params[:id])
-	end
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:success] = "Product successfully created"
+      redirect_to @product
+    else
+      flash[:error] = "Something went wrong"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def product_params
+      params.expect(product: [ :name ])
+    end
 end
